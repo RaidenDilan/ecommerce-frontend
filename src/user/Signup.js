c50f24b3e198eea145e2a979c4860da495fce1af
RaidenDilan <raiden18@me.com>
2020-08-05 21:16:20 +0100

    Shared layout component

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import Layout from '../core/Layout';
 
-const Signup = () => <div>SignUp</div>;
+const Signup = () => (
+  <Layout
+    title='Signup'
+    description='Signup to Node React E-commerce App'>
+    ...
+  </Layout>
+);
 
 export default Signup;
+
+
