05cb2f799a1fcfcf5e0fdd39e876bd0d74e0589d
RaidenDilan <raiden18@me.com>
2020-08-05 21:46:33 +0100

    User signout

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
+import { signout } from '../auth';
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) return { color: '#ff9900' };
@@ -34,6 +35,15 @@ const Menu = ({ history }) => (
           to='/signup'>
           Signup
         </Link>
+      </li>
+
+      <li className='nav-item'>
+        <span
+          className='nav-link'
+          style={ { cursor: 'pointer', color: '#ffffff' } }
+          onClick={ () => signout(() => history.push('/')) }>
+          Signout
+        </span>
       </li>
     </ul>
   </div>
