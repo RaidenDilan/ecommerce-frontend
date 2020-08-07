c276919095ae172cb16d8083ed3ef39a5ff26f78
RaidenDilan <raiden18@me.com>
2020-08-07 20:09:55 +0100

    Add to cart

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import { addItem } from './cartHelpers';
 
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false);
+
   const showViewButton = showViewProductButton => {
     return (
       showViewProductButton && (
@@ -18,18 +21,37 @@ const Card = ({ product, showViewProductButton = true }) => {
     );
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = redirect => {
+    if (redirect) return <Redirect to='/cart' />;
+  };
+
   const showAddToCartButton = () => {
-    return (<button className='btn btn-outline-warning mt-2 mb-2'>Add to cart</button>);
+    return (
+      <button
+        onClick={ addToCart }
+        className='btn btn-outline-warning mt-2 mb-2'>
+        Add to cart
+      </button>
+    );
   };
 
   const showStock = quantity => {
-    return quantity > 0 ? (<span className='badge badge-primary badge-pill'>In Stock</span>) : (<span className='badge badge-primary badge-pill'>Out of Stock</span>);
+    return quantity > 0
+      ? (<span className='badge badge-primary badge-pill'>In Stock</span>)
+      : (<span className='badge badge-primary badge-pill'>Out of Stock</span>);
   };
 
   return (
     <div className='card'>
       <div className='card-header name'>{ product.name }</div>
       <div className='card-body'>
+        { shouldRedirect(redirect) }
         <ShowImage
           item={ product }
           url='product' />
