beb73ef917b49eab8c1f8d5c2b99ef62ac40d499
RaidenDilan <raiden18@me.com>
2020-08-07 20:29:40 +0100

    Show products in cart page

--- src/Routes.js
+++ src/Routes.js
@@ -11,6 +11,7 @@ import AddCategory from './admin/AddCategory';
 import AddProduct from './admin/AddProduct';
 import Shop from './core/Shop';
 import Product from './core/Product';
+import Cart from './core/Cart';
 
 const Routes = () => {
   return (
@@ -52,6 +53,10 @@ const Routes = () => {
           path='/product/:productId'
           exact
           component={ Product } />
+        <Route
+          path='/cart'
+          exact
+          component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/core/cartHelpers.js
+++ src/core/cartHelpers.js
@@ -21,8 +21,14 @@ export const addItem = (item, next) => {
 
 export const itemTotal = () => {
   if (typeof window !== 'undefined') {
-    if (localStorage.getItem('cart')) 
-      return JSON.parse(localStorage.getItem('cart')).length;
+    if (localStorage.getItem('cart')) return JSON.parse(localStorage.getItem('cart')).length;
   }
   return 0;
+};
+
+export const getCart = () => {
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('cart')) return JSON.parse(localStorage.getItem('cart'));
+  }
+  return [];
 };
