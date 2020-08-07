b291cec2fc4392e5d587f9e1de2470fc9f3e03cc
RaidenDilan <raiden18@me.com>
2020-08-07 14:24:35 +0100

    Single Product Page + Related Products

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import AdminDashboard from './user/AdminDashboard';
 import AddCategory from './admin/AddCategory';
 import AddProduct from './admin/AddProduct';
 import Shop from './core/Shop';
+import Product from './core/Product';
 
 const Routes = () => {
   return (
@@ -47,6 +48,10 @@ const Routes = () => {
           path='/create/product'
           exact
           component={ AddProduct } />
+        <Route
+          path='/product/:productId'
+          exact
+          component={ Product } />
       </Switch>
     </BrowserRouter>
   );
